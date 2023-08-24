2acbc7811fceb2552ee5aa6fd93b7db0470e61ff
Kamrul Hasan <kamrul444216@gmail.com>
2023-08-25 01:33:14 +0600

    Header done

---
+++ src/app/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { FaBars } from "react-icons/fa";
+import NavLinks from "./NavLinks";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="grid grid-cols-3 p-10 items-center">
+        <FaBars className="cursor-pointer" />
+        <Link href={"/"} prefetch={false}>
+          <h1 className="font-serif text-2xl md:text-4xl text-center">
+            The <span className="underline decoration-6 decoration-orange-400">Current</span> News
+          </h1>
+        </Link>
+        <div className="flex items-center justify-end space-x-2">
+            {/* Dark Button  */}
+          <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
+            Subscribe Now
+          </button>
+        </div>
+      </div>
+      {/* NavLinks  */}
+      <NavLinks/>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-7xl font-bold'>Home Page</h1>
+      <h1 className='text-4xl font-bold'>Home Page</h1>
     </div>
   )
 }
